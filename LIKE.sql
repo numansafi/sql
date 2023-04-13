a09ba389240fe5996a5459bb106ad14388c1d819
Numan Safi <numansafi@live.com>
2023-04-13 01:11:28 +0100

    ädd .gitignore and BETWEEN

--- LIKE.sql
+++
@@ -1,7 +0,0 @@
--- LOVE anywhere in the title
-SELECT [title] FROM longlist
-WHERE title LIKE '%LOVE%';
-
--- Title with 'The' at the start of the title
-SELECT [title] FROM longlist
-WHERE title LIKE 'THE %';
